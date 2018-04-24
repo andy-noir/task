efa655f0e5a13bcba66f5dab9314d7d646b6217d
Andrey Gaisinskii <andy2k10@yandex.ru>
2018-04-24 11:04:41 +0300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass        = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
+
+    browserSync.init({
+        server: "./src"
+    });
+
+    gulp.watch("src/scss/*.scss", ['sass']);
+    gulp.watch("src/*.html").on('change', browserSync.reload);
+});
+
+// Compile sass into CSS & auto-inject into browsers + autoprefixer
+gulp.task('sass', function() {
+    return gulp.src("src/scss/*.scss")
+        .pipe(sass())
+        .pipe(gulp.dest("src/css"))
+        .pipe(autoprefixer({
+          browsers: ['Chrome >= 40', 'ie >= 9', 'FireFox >= 40', 'Safari >= 7']
+        }))
+        .pipe(gulp.dest("src/css"))
+        .pipe(browserSync.stream());
+});
+
+
+gulp.task('default', ['serve']);
