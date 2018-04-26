373d5b5a4eb01791afddb8478128b5a920de373e
Andrey Gaisinskii <andy2k10@yandex.ru>
2018-04-26 19:43:05 +0300

    almost done with video section. needs polishing

---
+++ js/main.js
@@ -0,0 +1,66 @@
+//---------------------------------
+// pochti ukral - pochti sdelal :)
+//---------------------------------
+
+
+// Load the IFrame Player API code asynchronously.
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/player_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// Replace the 'ytplayer' element with an <iframe> and
+// YouTube player after the API code downloads.
+var player;
+function onYouTubePlayerAPIReady() {
+  player = new YT.Player('yt-player', {
+    height: '588',
+    width: '925',
+    videoId: 'Y8NS-iBrh_A',
+    autoplay: '1',
+  });
+}
+
+$(".video-preview").on("click", function() {
+
+    var that = this;
+    $(this).addClass('hidden');
+    setTimeout(function() {
+        $(that).css('z-index', -1)
+    }, 1000)
+
+    $('.yt-video').removeClass('hidden');
+
+  });
+
+// var player;
+
+// function initPlayer() {
+//     function onYouTubeIframeAPIReady() {
+//         player = new YT.Player('player', {
+//             height: '588',
+//             width: '925',
+//             videoId: 'Y8NS-iBrh_A',
+//             events: {
+//                 'onReady': onPlayerReady
+//             }
+//         });
+//     }
+
+//     function onPlayerReady(event) {
+//         event.target.playVideo();
+//     }
+
+//     $('.video-preview').on('click', function(event) {
+//         event.preventDefault();
+//         onYouTubeIframeAPIReady();
+
+//         var that = this;
+//         $(this).addClass('hidden');
+//         setTimeout(function() {
+//             $(that).css('z-index', -1)
+//         }, 1000)
+//     });
+// }
+
+// initPlayer();
